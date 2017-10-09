313f80486f641984585f89f6f208ef26bfc8b779
cesar mejia <analista4.guma@gmail.com>
2017-10-09 09:01:09 -0600

    primer commit

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,502 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void marcar(Button sender)
+        {
+
+            if (sender.BackColor == Color.RoyalBlue)
+            {
+                sender.BackColor = Color.Green;
+                sender.ForeColor = Color.White;
+            }
+            else
+            {
+                sender.BackColor = Color.RoyalBlue;
+                sender.ForeColor = Color.White;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button45_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button44_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button43_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button42_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button41_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button40_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button60_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button59_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button58_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button57_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button56_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button55_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button54_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button53_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button52_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button51_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button50_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button49_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button48_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button47_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button46_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button75_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button74_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button73_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button72_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button71_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button70_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button69_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button68_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button67_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button66_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button65_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button64_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button63_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button62_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button61_Click(object sender, EventArgs e)
+        {
+            Button boton1 = sender as Button;
+            marcar(boton1);
+        }
+
+        private void button76_Click(object sender, EventArgs e)
+        {
+            foreach (Control controlChotex in this.Controls)
+            {
+                if (controlChotex is Button)
+                    controlChotex.BackColor = Color.RoyalBlue;
+                    controlChotex.ForeColor = Color.White;
+            }
+        }
+
+        private void button77_Click(object sender, EventArgs e)
+        {
+            bingo bingo = new bingo();
+            //bingo.Show();
+            bingo.ShowDialog();//cesar por DIOS, como no pudiste hacer esto jajaja
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/bingo.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class bingo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bingo));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(483, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(483, 314);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bingo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 314);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "bingo";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "bingo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
